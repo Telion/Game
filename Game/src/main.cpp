1ae006fad9dbc70e37fbb05c35151468a2389e9e
Telion <christophermueller@sbcglobal.net>
2014-07-04 18:19:29 -0700

    Added SDL2 to the project.

--- Game/src/main.cpp
+++ Game/src/main.cpp
@@ -1,10 +1,23 @@
 #include "pch.h"
 
-#include <iostream>
+#include "SDL2/SDL.h"
 
 int main(int argc, char** argv)
 {
-	std::cout << "Hello Tina!" << std::endl;
+	SDL_Init(SDL_INIT_AUDIO | SDL_INIT_EVENTS | SDL_INIT_GAMECONTROLLER | SDL_INIT_VIDEO);
+
+	SDL_Window* window = SDL_CreateWindow("Tina game!", 50, 50, 800, 600, 0);
+	
+	SDL_Event event;
+	while(SDL_WaitEvent(&event))
+	{
+		switch(event.type)
+		{
+		case SDL_QUIT:
+			goto end;
+		}
+	}
+end:;
 
 	return 0;
 }
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 Game
 ====
+
+
+Dumb Legal Stuff
+================
+
+Portions of this software are copyright © 2013 The FreeType Project (www.freetype.org).  All rights reserved.
