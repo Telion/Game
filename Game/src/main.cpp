6881300c4919161c48fc9d6f09ddcb69876a3e4f
Telion <christophermueller@sbcglobal.net>
2014-07-04 19:12:35 -0700

    Added support for boost, and added a test of boost::serialization to main.cpp.  You have to put your boost 1.55 directory in the environment variable "BoostDirectory".

--- Game/src/main.cpp
+++ Game/src/main.cpp
@@ -1,9 +1,34 @@
 #include "pch.h"
 
 #include "SDL2/SDL.h"
+#include <iostream>
+#include "boost/archive/text_oarchive.hpp"
+#include "boost/serialization/serialization.hpp"
+
+class SerializableTest
+{
+public:
+	int a;
+	std::string str;
+
+	template<class Archive>
+	void serialize(Archive & ar, const unsigned int version)
+	{
+		ar & a;
+		ar & str;
+	}
+};
 
 int main(int argc, char** argv)
 {
+	// Test the Boost library before creating a window.
+	// This requires that boost::serialization was fixed manually.
+	boost::archive::text_oarchive archive(std::cout);
+	SerializableTest st;
+	st.a = 25;
+	st.str = "Hello, Tina!";
+	archive & st;
+
 	SDL_Init(SDL_INIT_AUDIO | SDL_INIT_EVENTS | SDL_INIT_GAMECONTROLLER | SDL_INIT_VIDEO);
 
 	SDL_Window* window = SDL_CreateWindow("Tina game!", 50, 50, 800, 600, 0);
