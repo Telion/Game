b0fc23ef59ee4eb8000118db53a9d8eb2a66a785
Telion <christophermueller@sbcglobal.net>
2014-07-04 17:59:32 -0700

    Created the project.

---
+++ Game/pch.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "targetver.h"
+
+//Any commonly used includes (such as standard library files) should go here
---
+++ Game/src/main.cpp
@@ -0,0 +1,10 @@
+#include "pch.h"
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+	std::cout << "Hello Tina!" << std::endl;
+
+	return 0;
+}
